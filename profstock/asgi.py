77ed15a066f96fa9f5ed95698c67288e86634a8b
Ubuntu <ubuntu@ip-172-31-25-105.us-east-2.compute.internal>
2022-02-25 00:44:42 +0000

    Fixed incorrect module names

--- profstock/asgi.py
+++ profstock/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for proftstock project.
+ASGI config for profstock project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proftstock.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profstock.settings')
 
 application = get_asgi_application()
