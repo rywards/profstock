afd4b1c9f3e43d9d3fd6aa6132fd1723272f0951
Ryan E <rywards@ymail.com>
2022-03-01 21:01:07 -0500

    Updating branches

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
-from flask_breadcrumbs import Breadcrumbs, register_breadcrumb 
 
 app = Flask(__name__)
 
+# route decorators
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("Home.html")
 
 @app.route("/")
 def signup():
     return render_template("SignUpPage.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 app.static_folder = 'static'
