0370f6264802e20cef443afbe4687295a474519f
benjamin200029 <bade4943@Stu.raritanval.edu>
2022-03-03 12:03:21 -0500

    app.py fix

--- app.py
+++ app.py
@@ -4,14 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/")
-def home():
     return render_template("Home.html")
 
-@app.route("/")
-def signup():
-    return render_template("SignUpPage.html")
 
 app.static_folder = 'static'
