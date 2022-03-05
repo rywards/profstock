29a528f42dd175f04b5ebafdc0a97b5ea5014157
servis38 <servis38@students.rowan.edu>
2022-03-05 13:03:43 -0500

    authentication with login/logout buttons

--- profstock/src/App.js
+++ profstock/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+
+/*
+Resources for getting authentication working
+
+https://auth0.com/blog/complete-guide-to-react-user-authentication/
+https://www.youtube.com/watch?v=MqczHS3Z2bc
+https://www.youtube.com/watch?v=aRBgA8N0ioM&t=373s
+
+*/
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <LoginButton/>
+    <LogoutButton/>
+    </>
   );
 }
 
