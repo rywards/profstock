cb6220557ba7bf63a9eaaafebf4582aac5493540
servis38 <servis38@students.rowan.edu>
2022-03-21 12:13:17 -0400

    buttons

--- profstock/src/App.js
+++ profstock/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import { useAuth0 } from "@auth0/auth0-react";
 
 /*
 Resources for getting authentication working
@@ -12,10 +13,20 @@ https://www.youtube.com/watch?v=aRBgA8N0ioM&t=373s
 */
 
 function App() {
+  const { user } = useAuth0();
+  //const userInfo = JSON.stringify(user, null, 2);
+  //const primaryKey = userInfo.sub;
+
+  //console.log(primaryKey);
   return (
     <>
     <LoginButton/>
     <LogoutButton/>
+    <div className="row">
+        <pre className="col-12 text-light bg-dark p-4">
+          {JSON.stringify(user, null, 2)}
+        </pre>
+    </div>
     </>
   );
 }
