a35aff8cceb5f6f01577ebbd5105b32c8c24ebf7
johnst35 <johnst35@students.rowan.edu>
2022-04-09 13:09:41 -0400

    Update App.js

--- profstock/src/App.js
+++ profstock/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import PersonList from './components/PersonList.js';
+import SubLinks from './components/SubLink.js';
 
 function App() {
   return (
