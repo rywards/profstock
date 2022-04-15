8f779e3959e714631efede6cf1cdcbbe5bfe93a9
JimmyT089 <71527169+JimmyT089@users.noreply.github.com>
2022-04-15 14:37:51 -0400

    give portfolio the sublin component
    portfolio now have the sublink. thanks to bens code.

--- profstock/src/App.js
+++ profstock/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
-import Stock from './portfolio';
+import Stock from './components/portfolio';
+import SubLinks from './components/sublink';
 
 function App() {
   return (
+    <div ClassName="StockHome">
+      <header>
+        <h1>ProfStock</h1>
+      </header>
+      <SubLinks />
+      <Stock />
 
-        <Stock />
-    
- 
+    </div>
+
   );
 }
 
---
+++ profstock/src/components/sublink.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import axios from 'axios';
+
+<>
+    <script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+</>
+
+
+export default class SubLinks extends React.Component {
+    render() {
+        return (
+            <h2>
+                <ul id="subLinkUl">
+                    <li id="subLinkLi"><a className="active" href="{{ url_for('home') }}">Home</a></li>
+                    <li id="subLinkLi"><a href="portfolio">Portfolio</a></li>
+                    <li id="subLinkLi"><a href="watchlist">Watchlist</a></li>
+                    <li id="subLinkLi" style={{ float: "right" }}>
+                        <form action="/stockinfo" method="post">
+                            <input type="text" name="ticker" placeholder="Enter ticker" />
+                            <input type="submit" value="Search" />
+                        </form>
+                    </li>
+                    if(session) {
+                        <li id="subLinkLi" style={{ float: "right" }}><a href="{{url_for('logout')}}">Logout</a></li>
+                    } else {
+                        <li id="subLinkLi" style={{ float: "right" }}><a href="{{url_for('login')}}">Login</a></li>
+                    }
+                </ul>
+            </h2>
+        );
+    }
+}
