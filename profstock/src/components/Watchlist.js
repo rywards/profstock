611a107b6dca2cea85ec09372c779dfa6e64913d
johnst35 <johnst35@students.rowan.edu>
2022-04-19 10:40:24 -0400

    Create Watchlist.js

---
+++ profstock/src/components/Watchlist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import './SubLinks.js';
+
+<>
+<script src="https://unpkg.com/react@17/umd/react.development.js" crossorigin></script>
+<script src="https://unpkg.com/react-dom@17/umd/react-dom.development.js" crossorigin></script>
+<script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+</>
+
+
+export default class WatchInfo extends React.Component {
+    
+}
+
