54ecf64f858ffadb886c2b7c2eef966932eea91f
Ryan <rywards@ymail.com>
2022-04-20 16:53:48 -0400

    Getting front working with my branch

--- profstock/src/App.js
+++ profstock/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-import PersonList from './components/PersonList.js';
 import SubLinks from './components/SubLinks';
 import StockList from './components/StockList';
 
